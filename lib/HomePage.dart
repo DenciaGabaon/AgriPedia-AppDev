2d81ae64ded5f80de5283277ec29780091f55a74
JkyleAgullo <jkyleagullo@gmail.com>
2024-02-02 16:36:40 +0800

    Modified homepage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -4,6 +4,11 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+
+
+
 //import 'package:permission_handler/permission_handler.dart';
 //import 'package:tflite_flutter/tflite_flutter.dart'
 
@@ -17,16 +22,108 @@ class MyHomePage extends StatefulWidget {
 class Home extends State<MyHomePage>{
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,//Color.fromRGBO(245, 245, 219, 1),
-      body: SafeArea(
+    return Scaffold(
+      backgroundColor: Colors.white, //Color.fromRGBO(245, 245, 219, 1),
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('AgriPedia'),
+            SvgPicture.asset(
+              'assets/HomeNav.svg',
+              height: 30,
+            ),
+            const SizedBox(width:8),
+          ],
+        ),
+        centerTitle: false,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        /*title: const Text('AgriPedia'),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        backgroundColor: Colors.red,*/
+      ),
+      /*body: SafeArea(
         child: Padding(
           padding: EdgeInsets.fromLTRB(18, 18, 18, 18),
-          child: Center(
-            child: Text('AgrpediMolipet'),
+          child: Align(
+            alignment: Alignment.topCenter,
+          child: Container(
+            padding: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            width: 500,
+            height: 165,
+            child: Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0)
+                  ),
+                  width: 125,
+                  height: 150,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0)
+                  )
+                ),
+              ],
+            )
+            ),
           ),
         ),
+      ),*/
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+        children: [
+          const SizedBox(width: 12),
+          buildCard(),
+          const SizedBox(width: 12),
+          buildCard(),
+          const SizedBox(width: 12),
+          buildCard(),
+          const SizedBox(width: 12),
+          buildCard(),
+          const SizedBox(width: 12)
+        ],
+      ),
       ),
     );
   }
+  Widget buildCard() => Container(
+    decoration: BoxDecoration(
+      color: Colors.green,
+      borderRadius: BorderRadius.circular(20.0)
+    ),
+    width: 125,
+    height: 125,
+    child: Column(
+      children: [
+        const Padding(padding: EdgeInsets.all(3.0)),
+        Image.asset('assets/tomatoes.png'),
+        const Text(
+          'Tomato Tornado',
+          style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        const Text(
+          'Status: Online'
+        ),
+        Container(
+          height: 20,
+          width: 100,
+          decoration: BoxDecoration(
+            color: Colors.lightGreen,
+            borderRadius: BorderRadius.circular(20.0)
+          ),
+        ),
+      ],
+    )
+  );
 }
